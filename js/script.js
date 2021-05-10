bc1f757710784dd42453fb93af2ba1ee6473b723
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-10 15:26:41 +0200

    terminato esercizio

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+
+var studente = {
+    nome:"Beatrice",
+    cognome:"Baccano",
+    eta:31
+};
+
+console.log(studente);
+
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+for (var key in studente){
+    console.log(studente[key]);
+}
+// Creare un array di oggetti di studenti.
+
+var studenti = [
+    {
+        nome:"Beatrice",
+        cognome:"Baccano",
+        eta:31
+    },
+    {
+        nome:"Pinco",
+        cognome:"Pallino",
+        eta:31
+    },
+    {
+        nome:"Mario",
+        cognome:"Rossi",
+        eta:31
+    }
+];
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all'utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell'ordine: nome, cognome e età.
+
+
+var studenteNuovo = {
+    nome:prompt("Inserisci il tuo nome"),
+    cognome:prompt("Inserisci il tuo cognome"),
+    eta:parseInt(prompt("Inserisci la tua età"))
+}
+
+studenti.push(studenteNuovo);
+
+for (var i = 0; i< studenti.length; i++){
+    console.log("Nome studente",studenti[i].nome);
+    console.log("Cognome studente",studenti[i].cognome);
+}
+
+
